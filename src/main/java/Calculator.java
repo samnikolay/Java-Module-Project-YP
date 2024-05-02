4630ca02170b6fa6d21bf0933ee61109b7216441
samnikolay <sam.nikolay@gmail.com>
2024-05-02 08:31:53 +0300

    Коммит после исправления замечаний ревьювера. Практическая работа №1.

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,14 +1,67 @@
+import java.util.ArrayList;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Calculator {
     /* Класс хронит в себе добавленные товары и общую сумму за эти товары */
-    String goodsString = "Добавленные товары:\n"; // Задаем начальную строку
+
+    ArrayList<Goods> goodsList = new ArrayList<>(); // Создаем массив для имен продуктов
     double summa; // Создаем переменную под общую сумму
-    Formatter formatter = new Formatter(); // Создаем экземпляр класса форматтера
-    void addGoods(String name,double price) {
-        goodsString = goodsString + name +" - "+ price +" "+ formatter.wordEnd(price) +"\n"; // Добавляем название товара и цену в строку
-        summa += price; // Складываем цены в общую сумму
+
+    public  Calculator() {
+        Scanner scanner = new Scanner(System.in); // Подключаем сканер
+        while(true) {
+            String goodsName;
+            System.out.println("Введите товар или команду [Завершить]:"); // Просим пользователя ввести название товара или завершить подсчет
+            goodsName = scanner.next(); // Сканируем введенный текст
+
+            if(goodsName.equalsIgnoreCase("Завершить")) { // Сравниваем строку с командой завершения
+                break; // если пользователь ввел "Завершить", то останавливаем цикл
+            }
+            else if (goodsName.trim().isEmpty()) { // Режем пробелы и проверяем на пустоту
+                System.out.println("Ошибка! Название товара не должно быть пустым."); // Если пользователь не ввел название и нажал Enter
+                continue;
+            }
+            else { // Иначе (если все ОК) добавляем товар и просим пользователя ввести цену товара
+                Goods goods = new Goods(goodsName);
+                while(true) { // Цикл нужен, чтобы запрашивать цену товара у пользователя, пока тот не введет корректное значение
+                    double price; // Для цены товаров
+                    try {
+                        Scanner scannerPrice = new Scanner(System.in); // Подключаем сканер (если этого не сделать, до зацикливается)
+                        scannerPrice.useLocale(Locale.US); // В ТЗ сказано, что при вводе/выводе число должно разделяться точкой
+                        System.out.println("Введите его цену в формате Рубли.Копейки:"); // Спрашиваем цену у пользователя
+                        price = scannerPrice.nextDouble(); // Сканирует ответ
+
+                        if(price >=0) { // если цена корректная
+                            goods.price = price;
+                            goodsList.add(goods);
+                            summa += price;
+                            break; // Останавливаем цикл с запросом цены
+                        }
+                        else { // Иначе выводим сообщение об ошибке
+                            System.out.println("Ошибка. Цена не может быть отрицательной");
+                        }
+
+                    }
+                    catch(Exception ex) {
+                        System.out.println("Ошибка! Цена должна быть числом в формате Рубли.Копейки.");
+                    }
+                }
+            }
+        }
     }
 
-    double divideSumma(double summa, int people) {
-        return summa / people; // Возвращаем результат (такое сокращение предложила IDE)
+    void printGoods() {
+        Formatter formatter = new Formatter(); // Создаем экземпляр класса форматтера
+
+        System.out.println("Добавленные товары:\n");
+        for (Goods goods : goodsList) {
+            System.out.println(goods.name + "-"+ formatter.numberFormat(goods.price));
+        }
+        System.out.println("Общая сумма "+ formatter.numberFormat(summa)); // Выводим общую сумму за все товары
+    }
+    void divideSumma(int people) {
+        Formatter formatter = new Formatter(); // Создаем экземпляр класса форматтера
+        System.out.println("Каждый должен заплатить "+ formatter.numberFormat(summa / people)); // Выводим результат
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,54 +3,9 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int peopleNumber; // Кол-во людей для разделения счета
-        String goods; // Для названий товаров
-        double price; // Для цены товаров
-
-        Scanner scanner = new Scanner(System.in); // Подключаем сканер
-        scanner.useLocale(Locale.US); // В ТЗ сказано, что при вводе/выводе число должно разделяться точкой
+        PeopleCounter peopleCounter = new PeopleCounter(); // Создаем экземпляр класса счетчика людей
         Calculator calc = new Calculator(); // Создаем экземпляр класса калькулятора
-        Formatter formatter = new Formatter(); // Создаем экземпляр класса форматтера
-
-        while(true) {
-            System.out.println("На скольких человек необходимо разделить счёт"); // Задаем вопрос пользователю
-
-            peopleNumber = scanner.nextInt(); // Сканируем его ответ
-            if (peopleNumber <= 1) { // Проверяем ответ на корректность
-                System.out.println("Ошибка. Нельзя разделить счет на введенное количество людей");
-            }
-            else { // Если ответ корректный, то останавливаем цикл
-                break;
-            }
-        }
-
-        while(true) {
-            System.out.println("Введите товар или команду [Завершить]:"); // Просим пользователя ввести название товара или завершить подсчет
-
-            goods = scanner.next(); // Сканируем введенный текст
-            if(goods.equalsIgnoreCase("Завершить")) { // Сравниваем строку с командой завершения
-                break; // если пользователь ввел "Завершить", то останавливаем цикл
-            }
-            else { // Иначе просим пользователя ввести цену товара
-                while(true) { // Цикл нужен, чтобы запрашивать цену товара у пользователя, пока тот не введет корректное значение
-                    System.out.println("Введите его цену в формате Рубли.Копейки:"); // Спрашиваем цену у пользователя
-                    price = scanner.nextDouble(); // Сканирует ответ
-                    if(price >=0) { // если цена корректная
-                        calc.addGoods(goods,price); // добавляем товар в список
-                        break; // Останавливаем цикл с запросом цены
-                    }
-                    else { // Иначе выводим сообщение об ошибке
-                        System.out.println("Ошибка. Цена не может быть отрицательной");
-                    }
-                }
-            }
-        }
-
-        System.out.println(calc.goodsString); // Выводим строку со списком товаров
-
-        System.out.println("Общая сумма "+ formatter.numberFormat(calc.summa) +" "+ formatter.wordEnd(calc.summa)); // Выводим общую сумму за все товары
-        System.out.println("Каждый должен заплатить "+ formatter.numberFormat(calc.divideSumma(calc.summa, peopleNumber))+" "+
-                formatter.wordEnd(calc.divideSumma(calc.summa, peopleNumber))); // Выводим сумму, которую должен заплатить каждый
-
+        calc.printGoods(); // Выводим все добавленные товары на экран
+        calc.divideSumma(peopleCounter.peopleNumber); // Выводим итоговую сумму на одного человека
     }
 }
